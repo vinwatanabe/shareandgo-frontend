58bc127027fabed8b35fcdfd58f07d726b08ef5e
ed-uemura <117615570+ed-uemura@users.noreply.github.com>
2022-11-10 13:02:06 -0800

    Update user.jsx

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,6 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const USER_API = process.env.REACT_APP_BACKEND_URL+"user/";
+
 function User() {
-       return (
-       <h2>Users</h2>
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        const getUsers = async () =>{
+            try {
+                const response = await axios.get(USER_API+ 'viewall');
+                setUsers(response.data);
+            }catch (error) {
+                console.log(`URL: ${USER_API+ 'viewall'}\nERROR: ${error}`);
+                return "Error retrieving user data from DB"
+            }
+        };
+        getUsers();
+    }, []);
+    console.log(users);
+    return (
+        <>
+            <h2>Users</h2>
+            <div className="table-responsive">
+                <table className="table">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">FIRSTNAME</th>
+                        <th scope="col">LASTNAME</th>
+                        <th scope="col">EMAIL</th>
+                        <th scope="col">PHONE</th>
+                    </tr>
+                    { 
+                        users && users.length>0 && (
+                            users.map(
+                                (user) => 
+                                (
+                                    <tr>
+                                        <td>{user._id}</td>
+                                        <td>{user.firstname}</td>
+                                        <td>{user.lastname}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.phone}</td>
+                                    </tr>
+                                )
+                            )
+                        )
+                    }
+                </table>
+            </div>
+        </>
    )
 }
  
