0258789824e0c476376b8e4e1cfe62a7e521810f
Vinicius Watanabe <vinicius_watanabe@hotmail.com>
2022-11-16 06:25:29 -0800

    Adjustments created css files for components and adjustments made on SearchBar button

--- src/components/ButtonPrimary.js
+++ src/components/ButtonPrimary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/ButtonPrimary.css';
 
 const ButtonPrimary = ({ text, className, link, clickAction }) => {
 	return (
