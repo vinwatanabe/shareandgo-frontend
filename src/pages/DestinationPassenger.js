fe8bc4af4063022cc2e14633bc26807a774f68a8
Vinicius Watanabe <vinicius_watanabe@hotmail.com>
2022-11-16 05:04:30 -0800

    Finished DestinationPassenger page

--- src/pages/DestinationPassenger.js
+++ src/pages/DestinationPassenger.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ScheduleIcon from '../images/schedule.png';
+import ButtonPrimary from '../components/ButtonPrimary';
 
 const DestinationPassenger = () => {
+	const [values, setValues] = useState('');
+
+	function handleChange(event) {
+		const auxValues = { ...values };
+		auxValues[event.target.id] = event.target.value;
+		setValues(auxValues);
+	}
+
 	return (
 		<>
-			<h1>DestinationPassenger</h1>
+			<div className='text-center'>
+				<figure>
+					<img src={ScheduleIcon} alt='Schedule illustration' />
+				</figure>
+			</div>
+
+			<div className='text-center mt-5'>
+				<h2 className='text-title mb-4'>Let's schedule your ride</h2>
+
+				<form className='row col-12 col-md-6 g-4 mb-4 mx-auto'>
+					<div className='col-md-12'>
+						<input
+							type='text'
+							id='currentLocation'
+							placeholder='Where are you now?'
+							className='form-control'
+							onChange={(e) => handleChange(e)}
+						/>
+					</div>
+
+					<div className='col-md-12'>
+						<input
+							type='text'
+							id='destinationLocation'
+							placeholder='Where are you going?'
+							className='form-control'
+							onChange={(e) => handleChange(e)}
+						/>
+					</div>
+
+					<div className='col-md-6'>
+						<input
+							type='date'
+							id='rideDate'
+							placeholder='Select date'
+							className='form-control'
+							onChange={(e) => handleChange(e)}
+						/>
+					</div>
+
+					<div className='col-md-6'>
+						<input
+							type='time'
+							id='rideTime'
+							placeholder='Select time'
+							className='form-control'
+							onChange={(e) => handleChange(e)}
+						/>
+					</div>
+				</form>
+
+				<ButtonPrimary
+					text="Let's go"
+					className='col-6 col-md-3 mx-auto d-block'
+					link='/drivers-available'
+					clickAction=''
+				/>
+			</div>
 		</>
 	);
 };
