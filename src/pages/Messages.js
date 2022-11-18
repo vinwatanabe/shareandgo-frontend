f1fb7107d785861933ad36c895135a88bec076dd
Vinicius Watanabe <vinicius_watanabe@hotmail.com>
2022-11-18 13:45:05 -0800

    Finish Messages page

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import MessageCard from '../components/MessageCard';
+import DriverImage from '../images/profile-picture-mockup.jpg';
 
 const Messages = () => {
+	// Delete after implementation
+	const messages = [
+		{
+			name: 'Mathew Pitts',
+			destination: 'Abbotsford, BC',
+			date: '10/15/2022',
+			time: '9:45am',
+		},
+		{
+			name: 'Betty Wallace',
+			destination: 'New Westminster, BC',
+			date: '10/12/2022',
+			time: '4:22pm',
+		},
+		{
+			name: 'Harvey Specter',
+			destination: 'Mapple Ridge, BC',
+			date: '10/02/2022',
+			time: '11:18am',
+		},
+	];
+
 	return (
 		<>
-			<h1>Messages</h1>
+			<div>
+				<h2 className='text-title text-center mb-4'>Your messages(2)</h2>
+				{messages.map((user) => {
+					return (
+						<div className='col-12 col-sm-10 col-md-8 col-lg-5 col-xl-4 mx-auto mb-3'>
+							<MessageCard
+								driverPhoto={DriverImage}
+								driverName={user.name}
+								destination={user.destination}
+								date={user.date}
+								time={user.time}
+								link='/ride-info'
+							/>
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 };
