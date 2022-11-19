9b436f2f9c095e606412a44b6ea9ace3f16c845d
ed-uemura <117615570+ed-uemura@users.noreply.github.com>
2022-11-19 15:51:48 -0800

    Update Registration.js

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,14 +1,112 @@
 import React, { useState } from 'react';
 import ButtonPrimary from '../components/ButtonPrimary';
+import InputMask from "react-input-mask";
+import axios from 'axios';
+
+// Canadian postal code mask
+const firstLetter = /(?!.*[DFIOQU])[A-VXY]/i;
+const letter = /(?!.*[DFIOQU])[A-Z]/i;
+const digit = /[0-9]/;
+const mask = [firstLetter, digit, letter, " ", digit, letter, digit];
+// Session and page-transition variables
+const localStorage = window.localStorage;
 
 const Registration = () => {
 	const [values, setValues] = useState('');
+    const [error, setError] = useState("");
 
 	function handleChange(event) {
 		const auxValues = { ...values };
 		auxValues[event.target.id] = event.target.value;
 		setValues(auxValues);
 	}
+
+    const handleSubmit = async (e) => {
+		e.preventDefault();
+		// Fields Validation
+		if(!values.firstName) 
+			setError('Please enter your first name');
+		else if(!values.lastName) 
+			setError('Please enter your last name');
+		else if(!values.email) 
+			setError('Please enter an email address');
+		else if(!values.phone) 
+			setError('Please enter your phone number');
+		else if(!values.accountType) 
+			setError('Please select an account type');
+		else if(!values.password) 
+			setError('Please enter a password');
+		else if(!values.confirmPassword) 
+			setError('Please confirm your password');
+		else if(values.confirmPassword !== values.password)
+			setError('Passwords do not match');
+		else {
+			setError('');
+			// Prepare user object
+			const user = {
+				firstname: values.firstName,
+				lastname: values.lastName,
+				email: values.email,
+				phone: values.phone,
+				city: values.city,
+				province: values.province,
+				accounttype: values.accountType,
+				password: values.password,
+			};
+			try {
+				// Send user object to server
+				const res = await axios.post(process.env.REACT_APP_BACKEND_URL+"user/create", user);
+				if(res.data.error) {
+					// If error, display error message
+					setError(res.data.error);
+					throw new Error(res.data.error);
+				}
+				else{
+					// If no error, save user data to session storage
+					localStorage.setItem("userToken",res.data.token);
+					localStorage.setItem("userID",res.data._id);
+					localStorage.setItem("userFirstName",values.firstName);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+			// Validate existing payment data: if exists, register payment method, if not, move forward
+			let payment = {};
+            if(values.accountType === 'passenger'){
+                if(values.nameCard && values.numberCard && values.expirationDate && values.csc && values.zipCode) {
+                    payment = {
+                        type: "card",
+                        name: values.nameCard,
+                        pan: values.numberCard,
+                        expiration: values.expirationCard,
+                        csc: values.csc,
+                        address_id: values.zipCard,
+                    };
+                }
+            }
+            else{
+                if(values.bankName && values.institutionNumber && values.transitNumber && values.accountNumber) {
+                    payment = {
+                        type: "bank",
+                        name: values.bankName,
+                        pan: values.accountNumber,
+                        institution: values.institutionNumber,
+                        transit: values.transitNumber
+                    };
+                }
+                try {	
+                    await axios.post(process.env.REACT_APP_BACKEND_URL+"payment/create/", payment);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+			// Check Session Storage
+			if(localStorage.getItem('userToken')) {
+				// redirect to main page
+				values.accountType === 'driver' ? window.location.href = '/main-driver' : window.location.href = '/main-passenger';
+			}
+		}
+	};
 
 	const passengerBank = (
 		<div>
@@ -25,7 +123,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-12'>
-					<input
+                    <InputMask 
+						mask='9999 9999 9999 9999'
+						maskPlaceholder='-'
 						type='text'
 						id='cardNumber'
 						placeholder='Card Number'
@@ -35,7 +135,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-md-6'>
-					<input
+                    <InputMask 
+						mask='99/99'
+						maskPlaceholder='mm/yy'
 						type='text'
 						id='expirationDate'
 						placeholder='Expiration Date'
@@ -45,7 +147,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-md-6'>
-					<input
+                    <InputMask 
+						mask='9999'
+						maskPlaceholder=' '
 						type='text'
 						id='csc'
 						placeholder='CSC'
@@ -53,13 +157,28 @@ const Registration = () => {
 						onChange={(e) => handleChange(e)}
 					/>
 				</div>
+
+                <div className='col-md-6'>
+					<InputMask 
+						mask={mask}
+						maskPlaceholder='___ ___'
+						type='text'
+						id='zipCode'
+						placeholder='ZIP Code'
+						className='form-control'
+						onChange={(e) => handleChange(e)}
+					/>
+				</div>
+                <div className="text-danger text-center" role="alert">
+					{error}
+				</div>
 			</form>
 
 			<ButtonPrimary
 				text='Register'
 				className='col-6 col-md-3 mx-auto d-block'
-				link='/main-passenger'
-				clickAction=''
+				// link='/main-passenger'
+				clickAction={handleSubmit}
 			/>
 		</div>
 	);
@@ -79,7 +198,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-md-6'>
-					<input
+                    <InputMask 
+						mask='999'
+						maskPlaceholder=' '
 						type='text'
 						id='institutionNumber'
 						placeholder='Institution #'
@@ -89,7 +210,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-md-6'>
-					<input
+                    <InputMask 
+						mask='99999'
+						maskPlaceholder=' '
 						type='text'
 						id='transitNumber'
 						placeholder='Transit #'
@@ -99,7 +222,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-12'>
-					<input
+                    <InputMask 
+						mask='999999999999'
+						maskPlaceholder=' '
 						type='text'
 						id='accountNumber'
 						placeholder='Account Number'
@@ -107,13 +232,16 @@ const Registration = () => {
 						onChange={(e) => handleChange(e)}
 					/>
 				</div>
+                <div className="text-danger text-center" role="alert">
+					{error}
+				</div>
 			</form>
 
 			<ButtonPrimary
 				text='Register'
 				className='col-6 col-md-3 mx-auto d-block'
-				link='/main-passenger'
-				clickAction=''
+				//link='/main-passenger'
+				clickAction={handleSubmit}
 			/>
 		</div>
 	);
@@ -153,7 +281,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-12'>
-					<input
+                    <InputMask 
+						mask='999-999-9999'
+						maskPlaceholder='_'
 						type='text'
 						id='phone'
 						placeholder='Phone Number'
@@ -173,7 +303,9 @@ const Registration = () => {
 				</div>
 
 				<div className='col-md-6'>
-					<input
+                    <InputMask 
+						mask='aa'
+						maskPlaceholder='-'
 						type='text'
 						id='province'
 						placeholder='Province'
