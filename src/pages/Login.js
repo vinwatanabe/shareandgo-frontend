51cb0651d5e3e5e16a5c1bda5024828357f4a2eb
ed-uemura <117615570+ed-uemura@users.noreply.github.com>
2022-11-19 17:30:53 -0800

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,8 +40,8 @@ const Login = () => {
 					// If no error, save user data to session storage
 					localStorage.setItem("userToken",res.data.token);
 					localStorage.setItem("userID",res.data._id);
-					localStorage.setItem("userFirstName",res.firstName);
-                    localStorage.setItem("userType",res.accountType);
+					localStorage.setItem("userFirstName",res.firstname);
+                    localStorage.setItem("userType",res.accounttype);
 					values.accountType === 'driver' ? window.location.href = '/main-driver' : window.location.href = '/main-passenger';
 				}
 			} catch (err) {
