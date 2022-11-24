cb3384532066d6c68b59410ac1213d3ecdea0592
ed-uemura <117615570+ed-uemura@users.noreply.github.com>
2022-11-23 16:19:12 -0800

    Update AuthContext.js
    Removed axios as all backend requests will be held by controller components

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,22 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+//import { useNavigate } from 'react-router-dom';
+//import axios from 'axios';
 
 const Context = createContext();
 
 const AuthContext = ({ children }) => {
+	const localStorage = window.localStorage;
 	const [authenticated, setAuthenticated] = useState(false);
 	const [loading, setLoading] = useState(true);
 	const [userData, setUserData] = useState({});
-	let navigate = useNavigate();
+	//const navigate = useNavigate();
 
 	useEffect(() => {
-		const token = localStorage.getItem('token');
+		const token = localStorage.getItem('userToken');
 
 		if (token) {
-			axios.defaults.header.post['Content-Type'] = 'application/json';
-			axios.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+			// axios.defaults.header.post['Content-Type'] = 'application/json';
+			// axios.defaults.headers = `x-access-token ${JSON.parse(token)}`;
 			setAuthenticated(true);
 		}
 
